999b8ec74aa9358a63774879d5fb06d083b371d6
Anderson Alcântara <anderson.alcantara@aluno.senac.rio>
2025-10-26 10:50:54 -0300

    Initial commit

---
+++ droverota/src/main/java/droverota/DroverotaApplication.java
@@ -0,0 +1,13 @@
+package droverota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroverotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroverotaApplication.class, args);
+	}
+
+}
